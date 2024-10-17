98bd9aee7f38d9e712a2cec98d8c386fa9345ac0
Dean Kerr <dean.kerr@gmail.com>
2024-10-18 00:17:41 +1100

    add package.json scripts

--- apps/esuite/next.config.mjs
+++ apps/esuite/next.config.mjs
@@ -1,12 +1,16 @@
 /** @type {import('next').NextConfig} */
 
+import bundleAnalyzer from '@next/bundle-analyzer'
+
 function getBackendUrl() {
   const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL
   if (!convexUrl) throw new Error('NEXT_PUBLIC_CONVEX_URL is not set')
   return convexUrl.replace('.cloud', '.site')
 }
 
-const backendUrl = getBackendUrl()
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+})
 
 const nextConfig = {
   images: {
@@ -20,9 +24,9 @@ const nextConfig = {
   rewrites: async () => [
     {
       source: '/convex/:slug',
-      destination: `${backendUrl}/i/:slug`,
+      destination: `${getBackendUrl()}/i/:slug`,
     },
   ],
 }
 
-export default nextConfig
+export default withBundleAnalyzer(nextConfig)
