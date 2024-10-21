e1f51198c32fbd7b992a05394b2103bdd5415ac8
Dean Kerr <dean.kerr@gmail.com>
2024-10-22 01:59:42 +1100

    remove unused env vars

--- packages/api/convex/lib/env.ts
+++ packages/api/convex/lib/env.ts
@@ -5,12 +5,9 @@ const schema = vb.object({
   CLERK_JWT_ISSUER_DOMAIN: vb.string(),
   CLERK_SECRET_KEY: vb.string(),
   CLERK_WEBHOOK_SECRET: vb.string(),
-  WORKERS_IMAGE_CLASSIFICATION_URL: vb.string(),
-  WORKERS_API_KEY: vb.string(),
-  // * endpoint keys
+
   ELEVENLABS_API_KEY: vb.string(),
   FAL_API_KEY: vb.string(),
-  OPENAI_API_KEY: vb.string(),
   OPENROUTER_API_KEY: vb.string(),
 })
 
