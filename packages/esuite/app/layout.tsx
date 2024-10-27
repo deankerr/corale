b9746ecf0a8594c8ff63f96ce729fabceccc7da6
Dean Kerr <dean.kerr@gmail.com>
2024-10-27 16:12:15 +1100

    update analytics configs to run in prod only

--- packages/esuite/app/layout.tsx
+++ packages/esuite/app/layout.tsx
@@ -1,9 +1,8 @@
-import './globals.css'
 import { LightboxProvider } from '@/components/lightbox/LightboxProvider'
 import { ClientProviders } from '@/components/util/ClientProviders'
 import { TailwindBreakpointIndicator } from '@/components/util/TailwindBreakpointIndicator'
 import { appConfig } from '@/config/config'
-import { cn, environment } from '@/lib/utils'
+import { cn } from '@/lib/utils'
 import { ClerkProvider } from '@clerk/nextjs'
 import { dark } from '@clerk/themes'
 import { OpenPanelComponent } from '@openpanel/nextjs'
@@ -13,6 +12,7 @@ import type { Metadata, Viewport } from 'next'
 import { Inter, JetBrains_Mono } from 'next/font/google'
 import { NuqsAdapter } from 'nuqs/adapters/next/app'
 import { Toaster } from 'sonner'
+import './globals.css'
 
 export const metadata: Metadata = {
   title: {
@@ -39,6 +39,8 @@ const mono = JetBrains_Mono({
   variable: '--font-mono',
 })
 
+const isProd = process.env.NODE_ENV === 'production'
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={cn('overscroll-none', sans.variable, mono.variable)}>
@@ -61,23 +63,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           </NuqsAdapter>
         </ClerkProvider>
         <TailwindBreakpointIndicator />
-        <Analytics />
+        <SpeedInsights />
+        <OpenPanelComponent
+          clientId="6f2e9c18-cc3c-49d1-a2ac-9e482c2e4a66"
+          trackScreenViews={isProd}
+          trackOutgoingLinks={isProd}
+          trackAttributes={true}
+        />
       </body>
     </html>
-  )
-}
-
-const Analytics = () => {
-  if (environment !== 'prod') return null
-  return (
-    <>
-      <SpeedInsights />
-      <OpenPanelComponent
-        clientId="6f2e9c18-cc3c-49d1-a2ac-9e482c2e4a66"
-        trackScreenViews={true}
-        trackAttributes={true}
-        trackOutgoingLinks={true}
-      />
-    </>
   )
 }
--- packages/esuite/sentry.client.config.ts
+++ packages/esuite/sentry.client.config.ts
@@ -2,15 +2,13 @@
 // The config you add here will be used whenever a users loads a page in their browser.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from "@sentry/nextjs";
+import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
-  dsn: "https://e8417f3f690091243c9e7bbec0df30b4@o4508166383009792.ingest.us.sentry.io/4508166396313600",
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
   // Add optional integrations for additional features
-  integrations: [
-    Sentry.replayIntegration(),
-  ],
+  integrations: [Sentry.replayIntegration()],
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
@@ -25,4 +23,6 @@ Sentry.init({
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
-});
+
+  enabled: process.env.NODE_ENV === 'production',
+})
