46431c6e4c72f6fda837acc8514ca00fa8a5ec55
Dean Kerr <dean.kerr@gmail.com>
2024-10-27 19:51:53 +1100

    fix home page navigation broken when outside of appshell

--- packages/esuite/components/pages/BlankPage.tsx
+++ packages/esuite/components/pages/BlankPage.tsx
@@ -8,7 +8,7 @@ export const BlankPage = () => {
     <div className="flex-col-center bg-gray-1 h-full w-full">
       <AppLogo className="text-gray-3 size-48" />
       <NavigationSheet>
-        <IconButton variant="ghost" aria-label="Open navigation sheet" className="md:invisible">
+        <IconButton variant="ghost" aria-label="Open navigation sheet" className="absolute left-2 top-2 md:invisible">
           <Icons.List size={20} />
         </IconButton>
       </NavigationSheet>
