1d0ee9a7e2e47e288263dee25080c7eac7a09ddd
Dean Kerr <dean.kerr@gmail.com>
2024-10-29 03:23:15 +1100

    move image model defaults to config

--- packages/esuite/config/config.ts
+++ packages/esuite/config/config.ts
@@ -5,3 +5,29 @@ export const appConfig = {
   siteTitle: `${devPrefix}e⋆suite`,
   siteDescription: "it's the e⋆suite",
 } as const
+
+export const imageModelConfig = {
+  defaultModel: 'fal-ai/flux/dev',
+  defaultInputs: {
+    negativePrompt: false,
+    loras: false,
+    maxQuantity: 4,
+    sizes: [
+      {
+        name: 'portrait',
+        width: 832,
+        height: 1216,
+      },
+      {
+        name: 'square',
+        width: 1024,
+        height: 1024,
+      },
+      {
+        name: 'landscape',
+        width: 1216,
+        height: 832,
+      },
+    ],
+  },
+}
