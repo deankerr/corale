bcae4005397d9379a0277957c0c8cd9d51b0f3d7
Dean Kerr <dean.kerr@gmail.com>
2024-11-29 16:33:44 +1100

    add apps/chat nextjs package

---
+++ apps/chat/app/layout.tsx
@@ -0,0 +1,36 @@
+import '@corale/ui/globals.css'
+import { ThemeProvider } from '@corale/ui/components/theme-provider'
+import type { Metadata } from 'next'
+import localFont from 'next/font/local'
+
+const geistSans = localFont({
+  src: './fonts/GeistVF.woff',
+  variable: '--font-geist-sans',
+  weight: '100 900',
+})
+const geistMono = localFont({
+  src: './fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
+  weight: '100 900',
+})
+
+export const metadata: Metadata = {
+  title: 'corale chat',
+  description: 'corale chat',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
