96f1573882b1aafc005566a31c3c0db7f2444265
Dean Kerr <dean.kerr@gmail.com>
2024-11-29 16:33:44 +1100

    add apps/chat nextjs package

---
+++ apps/chat/app/page.tsx
@@ -0,0 +1,9 @@
+import { AppLogoIcon } from '@corale/ui/icons/AppLogoIcon'
+
+export default function Home() {
+  return (
+    <div className="grid min-h-screen items-center justify-items-center p-8 sm:p-20">
+      <AppLogoIcon className="w-48 text-gray-500" />
+    </div>
+  )
+}
---
+++ apps/chat/next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  transpilePackages: ['@corale/ui'],
+}
+
+export default nextConfig
