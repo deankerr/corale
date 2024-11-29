5529c7dd8240898b9c63767b28f150d48ce9b9f0
Dean Kerr <dean.kerr@gmail.com>
2024-11-29 20:13:36 +1100

    add apps/chat-server convex package

---
+++ apps/chat-server/convex/schema.ts
@@ -0,0 +1,14 @@
+import { defineSchema, defineTable } from 'convex/server'
+import { v } from 'convex/values'
+
+export default defineSchema(
+  {
+    documents: defineTable({
+      fieldOne: v.string(),
+      fieldTwo: v.object({
+        subFieldOne: v.array(v.number()),
+      }),
+    }),
+  },
+  { schemaValidation: true },
+)
