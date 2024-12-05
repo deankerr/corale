0ddc80887c9f4b8840e47ee8469fc4ef33297390
Dean Kerr <dean.kerr@gmail.com>
2024-12-05 20:02:44 +1100

    chat provider

--- apps/chat/app/(main)/chat/page.tsx
+++ apps/chat/app/(main)/chat/page.tsx
@@ -1,6 +1,6 @@
-import { ChatThread } from '@/components/chat-thread/chat-thread'
+import { ChatThreadLegacy } from '@/components/chat-thread/chat-thread'
 import { Id } from '@corale/chat-server/dataModel'
 
 export default function Page() {
-  return <ChatThread threadId={'new' as Id<'threads'>} />
+  return <ChatThreadLegacy threadId={'new' as Id<'threads'>} />
 }
